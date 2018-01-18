8757f88d5b3ceea376e5e7849c6b9da72b37f921
Jan Nash <jnash@jnash.de>
2018-01-18 13:09:00 +0100

    Create DRFMetaResource

---
+++ DjangoRFAFInterface/Source/DRFResource/Protocols/DRFMetaResource.swift
@@ -0,0 +1,15 @@
+//
+//  DRFMetaResource.swift
+//  DjangoRFAFInterface
+//
+//  Created by Jan Nash (privat) on 18.01.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+public protocol DRFMetaResource {
+    static var defaultNode: DRFNode { get }
+}
