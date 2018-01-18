1ba369ce7f14946a132ae1d3fa3f16be8fe9b1f3
Jan Nash <jnash@jnash.de>
2018-01-18 14:27:25 +0100

    Create DRFPagination and DRFDefaultPagination

---
+++ DjangoRFAFInterface/Source/DRFResource/Protocols/DRFPagination.swift
@@ -0,0 +1,51 @@
+//
+//  DRFPagination.swift
+//  DjangoRFAFInterface
+//
+//  Created by Jan Nash (privat) on 18.01.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+
+// MARK: // Public
+// MARK: - DRFPagination
+public protocol DRFPagination {
+    var limit: UInt { get }
+    var next: URL { get }
+    var offset: UInt { get }
+    var previous: URL { get }
+    var totalCount: UInt { get }
+}
+
+
+// MARK: - DRFDefaultPagination
+public struct DRFDefaultPagination {
+    // Keys
+    public struct Keys {
+        public static let limit: String = "limit"
+        public static let next: String = "offset"
+        public static let offset: String = "offset"
+        public static let previous: String = "previous"
+        public static let totalCount: String = "total_count"
+    }
+    
+    // Init
+    public init(json: JSON) {
+        // ???: Better way than to force unwrap? How can we fail gracefully here? Throw?
+        self.limit = json[Keys.limit].uInt!
+        self.next = json[Keys.next].url!
+        self.offset = json[Keys.offset].uInt!
+        self.previous = json[Keys.previous].url!
+        self.totalCount = json[Keys.totalCount].uInt!
+    }
+    
+    // Public Variables
+    public var limit: UInt
+    public var next: URL
+    public var offset: UInt
+    public var previous: URL
+    public var totalCount: UInt
+}
