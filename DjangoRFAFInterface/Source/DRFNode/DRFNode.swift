fd57d424834756bfecc70738fc7397fb8a4b4a2b
Jan Nash <jnash@jnash.de>
2018-01-18 13:12:34 +0100

    Create DRFNode

---
+++ DjangoRFAFInterface/Source/DRFNode/DRFNode.swift
@@ -0,0 +1,14 @@
+//
+//  DRFNode.swift
+//  DjangoRFAFInterface
+//
+//  Created by Jan Nash (privat) on 18.01.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+public protocol DRFNode {
+}
