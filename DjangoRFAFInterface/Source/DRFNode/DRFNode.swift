04005caadc018e463cb308c89e80b3a3bba37c6c
Jan Nash <jnash@jnash.de>
2018-01-18 23:44:49 +0100

    Improve parameter generation

--- DjangoRFAFInterface/Source/DRFNode/DRFNode.swift
+++ DjangoRFAFInterface/Source/DRFNode/DRFNode.swift
@@ -15,30 +15,57 @@ import SwiftyJSON
 // MARK: Protocol Declaration
 public protocol DRFNode {
     var baseURL: URL { get }
-    func listEndpoint<T: DRFListGettable>(for resourceType: T.Type) -> URL
+    
     func parametersFrom(offset: UInt, limit: UInt, filters: [DRFFilter]) -> Parameters
+    func parametersFrom(offset: UInt, limit: UInt) -> Parameters
     func parametersFrom(filters: [DRFFilter]) -> Parameters
+    
+    func listEndpoint<T: DRFListGettable>(for resourceType: T.Type) -> URL
     func extractListResponse<T: DRFListGettable>(json: JSON) -> (DRFPagination, [T])
 }
 
 
 // MARK: Default Implementations
+// MARK: Parameter Generation
 public extension DRFNode {
     func parametersFrom(offset: UInt, limit: UInt, filters: [DRFFilter] = []) -> Parameters {
-        let paginationParameters: Parameters = [
-            DRFDefaultPagination.Keys.offset: offset,
-            DRFDefaultPagination.Keys.limit: limit
-        ]
-        let filterParameters: Parameters = self.parametersFrom(filters: filters)
-        var parameters: Parameters = [:]
-        paginationParameters.forEach({ parameters[$0.key] = $0.1 })
-        filterParameters.forEach({ parameters[$0.key] = $0.1 })
-        return parameters
+        return self._parametersFrom(offset: offset, limit: limit, filters: filters)
+    }
+    
+    func parametersFrom(offset: UInt, limit: UInt) -> Parameters {
+        return self._parametersFrom(offset: offset, limit: limit)
     }
     
     func parametersFrom(filters: [DRFFilter]) -> Parameters {
-        var parameters: Parameters = [:]
-        filters.forEach({ parameters[$0.key] = $0.value })
-        return parameters
+        return filters.reduce(into: [:], { $0[$1.key] = $1.value })
     }
 }
+
+
+// MARK: ListResponse Extraction
+public extension DRFNode {
+//    func extractListResponse<T: DRFListGettable>(json: JSON) -> (DRFPagination, [T]) {
+//
+//    }
+}
+
+
+// MARK: // Private
+// MARK: Parameter Generation Implementation
+private extension DRFNode {
+    func _parametersFrom(offset: UInt, limit: UInt, filters: [DRFFilter] = []) -> Parameters {
+        var parameters: Parameters = [:]
+        let writeToParameters: (String, Any) -> Void = { parameters[$0] = $1 }
+        self.parametersFrom(offset: offset, limit: limit).forEach(writeToParameters)
+        self.parametersFrom(filters: filters).forEach(writeToParameters)
+        return parameters
+    }
+    
+    func _parametersFrom(offset: UInt, limit: UInt) -> Parameters {
+        return [
+            DRFDefaultPagination.Keys.offset: offset,
+            DRFDefaultPagination.Keys.limit: limit
+        ]
+    }
+}
+
