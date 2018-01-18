9662f12f367fa598a49420cd8fad6a847f048ea0
Jan Nash <jnash@jnash.de>
2018-01-18 20:33:32 +0100

    Improve default implementations in DRFNode

--- DjangoRFAFInterface/Source/DRFNode/DRFNode.swift
+++ DjangoRFAFInterface/Source/DRFNode/DRFNode.swift
@@ -16,16 +16,26 @@ public protocol DRFNode {
     var baseURL: URL { get }
     func listEndpoint<T: DRFListGettable>(for resourceType: T.Type) -> URL
     func parametersFrom(offset: UInt, limit: UInt, filters: [DRFFilter]) -> Parameters
+    func parametersFrom(filters: [DRFFilter]) -> Parameters
 }
 
 
 // MARK: Default Implementations
 public extension DRFNode {
     func parametersFrom(offset: UInt, limit: UInt, filters: [DRFFilter] = []) -> Parameters {
-        var parameters: Parameters = [
+        let paginationParameters: Parameters = [
             DRFDefaultPagination.Keys.offset: offset,
             DRFDefaultPagination.Keys.limit: limit
         ]
+        let filterParameters: Parameters = self.parametersFrom(filters: filters)
+        var parameters: Parameters = [:]
+        paginationParameters.forEach({ parameters[$0.key] = $0.1 })
+        filterParameters.forEach({ parameters[$0.key] = $0.1 })
+        return parameters
+    }
+    
+    func parametersFrom(filters: [DRFFilter]) -> Parameters {
+        var parameters: Parameters = [:]
         filters.forEach({ parameters[$0.key] = $0.value })
         return parameters
     }
