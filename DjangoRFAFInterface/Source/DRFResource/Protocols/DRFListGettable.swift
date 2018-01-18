68c435d8623c950282bc138b6bbdb61b6d5acd6f
Jan Nash <jnash@jnash.de>
2018-01-18 13:44:06 +0100

    Create ListGettable

---
+++ DjangoRFAFInterface/Source/DRFResource/Protocols/DRFListGettable.swift
@@ -0,0 +1,60 @@
+//
+//  DRFListGettable.swift
+//  DjangoRFAFInterface
+//
+//  Created by Jan Nash (privat) on 18.01.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import Alamofire_SwiftyJSON
+
+
+// MARK: // Public
+// MARK: Protocol Declaration
+public protocol DRFListGettable: DRFMetaResource {
+    associatedtype ListResponseType: DRFListResponse
+    init(json: JSON)
+}
+
+
+// MARK: Default Implementation
+public extension DRFListGettable {
+    typealias ListRepsonseType = DRFDefaultListResponse
+}
+
+
+// MARK: Interface
+public extension DRFListGettable {
+    static func get(from node: DRFNode = Self.defaultNode, offset: UInt, limit: UInt) {
+        self._get(from: node, offset: offset, limit: limit)
+    }
+}
+
+
+// MARK: // Private
+private extension DRFListGettable {
+    static func _get(from node: DRFNode, offset: UInt, limit: UInt) {
+        let endpoint: URL = node.listEndpoint(for: self)
+        let parameters: [String : Any] = [:
+//            DRFPagination_.Keys.offset: offset,
+//            DRFPagination_.Keys.limit: limit
+        ]
+        
+        Alamofire.request(endpoint, parameters: parameters)
+            .validate()
+            .responseSwiftyJSON {
+                switch $0.result {
+                case let .failure(error):
+                    print(error)
+                case let .success(result):
+                    let listResponse: ListRepsonseType<Self> = ListRepsonseType(json: result)
+//                    let pagination: DRFPagination_ = listResponse.pagination
+                    let objects: [Self] = listResponse.results
+                    print(objects)
+                }
+        }
+    }
+}
--- DjangoRFAFInterface/Source/DRFResource/Protocols/DRFListResponse.swift
+++ DjangoRFAFInterface/Source/DRFResource/Protocols/DRFListResponse.swift
@@ -11,8 +11,33 @@ import SwiftyJSON
 
 
 // MARK: // Public
+// MARK: - DRFListResponse
 public protocol DRFListResponse {
-    associatedtype T: DRFListGettable
+    associatedtype ResultType: DRFListGettable
     init(json: JSON)
-    var results: [T] { get }
+    var results: [ResultType] { get }
+}
+
+
+// MARK: - DRFDefaultListResponse
+// MARK: Keys (static stored properties are not supported in generic types...)
+public struct DRFListResponseKeys {
+    static let meta: String = "meta"
+    static let results: String = "results"
+}
+
+
+// MARK: Struct Declaration
+public struct DRFDefaultListResponse<T: DRFListGettable>: DRFListResponse {
+    public typealias ResultType = T
+    
+    // Init
+    public init(json: JSON) {
+        //        self.pagination = DRFPagination_(json: json[DRFListResponseKeys.meta])
+        self.results = json[DRFListResponseKeys.results].map({ ResultType(json: $0.1) })
+    }
+    
+    // Variables
+    //    var pagination: DRFPagination
+    public var results: [T]
 }
