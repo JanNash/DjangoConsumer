ac7c97440d9812e972ff19863afd10a82bcc96bf
Jan Nash <jnash@jnash.de>
2018-01-21 23:19:33 +0100

    Create TestBackend

---
+++ DjangoRFAFInterfaceTests/TestBackend.swift
@@ -0,0 +1,16 @@
+//
+//  TestBackend.swift
+//  DjangoRFAFInterfaceTests
+//
+//  Created by Jan Nash (privat) on 21.01.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+import DjangoRFAFInterface
+
+
+// MARK: // Internal
+class TestBackend: MockBackend {
+    
+}
