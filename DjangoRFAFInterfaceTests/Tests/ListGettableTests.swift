f17353213f8286ef0f91333ec5fb4aafe6075bc2
Jan Nash <jnash@jnash.de>
2018-01-24 15:23:39 +0100

    Improve test ListGettableTests

--- DjangoRFAFInterfaceTests/Tests/ListGettableTests.swift
+++ DjangoRFAFInterfaceTests/Tests/ListGettableTests.swift
@@ -12,28 +12,40 @@ import DjangoRFAFInterface
 
 // MARK: // Internal
 class ListGettableTest: BaseTest {
+    // Variables
     var client: MockListGettableClient = MockListGettableClient()
+    
+    
+    // Setup / Teardown Overrides
+    override func setUp() {
+        super.setUp()
+        MockListGettable.clients.append(self.client)
+    }
+    
+    override func tearDown() {
+        MockListGettable.clients = []
+        super.tearDown()
+    }
 }
 
 
 // MARK: TestCases
 extension ListGettableTest {
     func testGettingWithoutAnyParameters() {
-        let expectation: XCTestExpectation = XCTestExpectation(description: "bla")
+        let expectation: XCTestExpectation = XCTestExpectation(
+            description: "Expected to successfully get some objects"
+        )
         
-        self.client.gotObjects_ = {
-            objects, success in
+        self.client.gotObjects_ = { _, _ in
             expectation.fulfill()
         }
         
         self.client.failedGettingObjects_ = {
-            failure in
-            XCTFail()
+            XCTFail("Failed getting objects with failure: \($0)")
         }
         
-        MockListGettable.clients.append(self.client)
         MockListGettable.get()
         
-        self.wait(for: [expectation], timeout: 10)
+        self.wait(for: [expectation], timeout: 1)
     }
 }
