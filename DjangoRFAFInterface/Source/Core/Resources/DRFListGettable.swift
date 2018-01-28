6d8049a30c4f55b385af54c3e6a9f1c6f2e0a048
Jan Nash <jnash@jnash.de>
2018-01-28 22:08:21 +0100

    Implement conditional conformances for DRFNeedsOAuth2
    The pattern looks quite promising, I think, humbly.

--- DjangoRFAFInterface/Source/Core/Resources/DRFListGettable.swift
+++ DjangoRFAFInterface/Source/Core/Resources/DRFListGettable.swift
@@ -22,6 +22,7 @@ public protocol DRFListGettable {
 
 
 // MARK: Default Implementations
+// MARK: where Self: DRFNeedsNoAuth
 public extension DRFListGettable where Self: DRFNeedsNoAuth {
     static func get(from node: DRFNode? = nil, offset: UInt = 0, limit: UInt = 0) {
         self._get(from: node, offset: offset, limit: limit, filters: [], addDefaultFilters: false)
@@ -29,7 +30,20 @@ public extension DRFListGettable where Self: DRFNeedsNoAuth {
 }
 
 
+// MARK: where Self: DRFNeedsOAuth2
+extension DRFListGettable where Self: DRFNeedsOAuth2 {
+    static func get(from node: DRFOAuth2Node? = nil, offset: UInt = 0, limit: UInt = 0) {
+        self._get(from: node, offset: offset, limit: limit, filters: [], addDefaultFilters: false)
+    }
+}
+
+
 // MARK: // Internal
+// These two default implementations are needed because DRFFilteredListGettable
+// reuses this function. I can't yet think of a use case for a similar pattern
+// for the (yet non-existent) Postables, Deletables, etc. since using filters
+// on list POSTs and DELETEs sounds quite odd to me.
+// MARK: where Self: DRFNeedsNoAuth
 extension DRFListGettable where Self: DRFNeedsNoAuth {
     static func get_(from node: DRFNode?, offset: UInt, limit: UInt, filters: [DRFFilterType], addDefaultFilters: Bool) {
         self._get(from: node, offset: offset, limit: limit, filters: filters, addDefaultFilters: addDefaultFilters)
@@ -37,7 +51,16 @@ extension DRFListGettable where Self: DRFNeedsNoAuth {
 }
 
 
+// MARK: where Self: DRFNeedsOAuth2
+extension DRFListGettable where Self: DRFNeedsOAuth2 {
+    static func get_(from node: DRFOAuth2Node?, offset: UInt, limit: UInt, filters: [DRFFilterType], addDefaultFilters: Bool) {
+        self._get(from: node, offset: offset, limit: limit, filters: filters, addDefaultFilters: addDefaultFilters)
+    }
+}
+
+
 // MARK: // Private
+// MARK: where Self: DRFNeedsNoAuth
 private extension DRFListGettable where Self: DRFNeedsNoAuth {
     static func _get(from node: DRFNode?, offset: UInt, limit: UInt, filters: [DRFFilterType], addDefaultFilters: Bool) {
         let node: DRFNode = node ?? self.defaultNode
@@ -66,6 +89,41 @@ private extension DRFListGettable where Self: DRFNeedsNoAuth {
                 )
                 self.clients.forEach({ $0.failedGettingObjects(with: failure) })
             }
+        )
+    }
+}
+
+
+// FIXME: This is just copied from above to test if the architecture works
+// MARK: where Self: DRFNeedsOAuth2
+private extension DRFListGettable where Self: DRFNeedsOAuth2 {
+    static func _get(from node: DRFOAuth2Node?, offset: UInt, limit: UInt, filters: [DRFFilterType], addDefaultFilters: Bool) {
+        let node: DRFOAuth2Node = node ?? self.defaultNode
+        let url: URL = node.absoluteListURL(for: self)
+        let limit: UInt = limit > 0 ? limit : node.defaultLimit(for: self)
+        
+        var allFilters: [DRFFilterType] = filters
+        if addDefaultFilters {
+            if let filteredListGettable = self as? DRFFilteredListGettable.Type {
+                allFilters += node.defaultFilters(for: filteredListGettable)
+            }
+        }
+        
+        let parameters: Parameters = node.parametersFrom(offset: offset, limit: limit, filters: allFilters)
+        ValidatedJSONRequest(url: url, parameters: parameters).fire(
+            onSuccess: { result in
+                let (pagination, objects): (DRFPagination, [Self]) = node.extractListResponse(for: self, from: result)
+                let success: GETObjectListSuccess = GETObjectListSuccess(
+                    node: node, responsePagination: pagination, offset: offset, limit: limit, filters: allFilters
+                )
+                self.clients.forEach({ $0.gotObjects(objects: objects, with: success) })
+        },
+            onFailure: { error in
+                let failure: GETObjectListFailure = GETObjectListFailure(
+                    objectType: self, node: node, error: error, offset: offset, limit: limit, filters: allFilters
+                )
+                self.clients.forEach({ $0.failedGettingObjects(with: failure) })
+        }
         )
     }
 }
