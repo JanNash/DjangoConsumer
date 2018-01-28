06020855205f5f068182c3bf68659943c060ce8b
Jan Nash <jnash@jnash.de>
2018-01-28 21:35:46 +0100

    Create DRFOAuht2Node and DRFOAuth2NeedyResource, improve groups

---
+++ DjangoRFAFInterface/Source/Core/Nodes/DRFOAuth2Node.swift
@@ -0,0 +1,15 @@
+//
+//  DRFOAuth2Node.swift
+//  DjangoRFAFInterface
+//
+//  Created by Jan Nash (privat) on 28.01.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+public protocol DRFOAuth2Node: DRFNode {
+    
+}
---
+++ DjangoRFAFInterface/Source/Core/Resources/DRFOAuth2NeedyResource.swift
@@ -0,0 +1,15 @@
+//
+//  DRFOAuth2NeedyResource.swift
+//  DjangoRFAFInterface
+//
+//  Created by Jan Nash (privat) on 28.01.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+public protocol DRFOAuth2NeedyResource {
+    static var defaultNode: DRFNode { get }
+}
