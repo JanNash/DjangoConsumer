ff5366de2c365b767864b6a931b539f3d6ea0c93
Jan Nash <jnash@jnash.de>
2018-02-03 15:34:46 +0100

    Privatize initializers in constants structs

--- DjangoRFAFInterface/Source/OAuth2/Constants/DRFOAuth2Constants.swift
+++ DjangoRFAFInterface/Source/OAuth2/Constants/DRFOAuth2Constants.swift
@@ -11,7 +11,10 @@ import Foundation
 
 // MARK: // Public
 public struct DRFOAuth2Constants {
+    private init() {}
+    
     public struct JSONKeys {
+        private init() {}
         public static let accessToken: String   = "access_token"
         public static let refreshToken: String  = "refresh_token"
         public static let expiresIn: String     = "expires_in"
@@ -20,19 +23,23 @@ public struct DRFOAuth2Constants {
     }
     
     public struct GrantTypes {
+        private init() {}
         public static let password: String      = "password"
         public static let refreshToken: String  = "refresh_token"
     }
     
     public struct Scopes {
+        private init() {}
         public static let readWrite: String     = "read write"
     }
     
     public struct HeaderFields {
+        private init() {}
         public static let authorization: String = "Authorization"
     }
     
     public struct HeaderValues {
+        private init() {}
         public static func basic(_ appSecret: String) -> String    { return "Basic \(appSecret)" }
         public static func bearer(_ accessToken: String) -> String { return "Bearer \(accessToken)" }
     }
