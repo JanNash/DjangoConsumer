250cb7b5daec415934d88befbfdff4723f914412
Jan Nash <jnash@jnash.de>
2018-02-06 15:11:46 +0100

    Mock test case

--- DjangoRFAFInterfaceTests/Tests/OAuth2Tests.swift
+++ DjangoRFAFInterfaceTests/Tests/OAuth2Tests.swift
@@ -13,13 +13,34 @@ import DjangoRFAFInterface
 
 // MARK: // Internal
 // MARK: Tests for OAuth2
-extension TestCase {
+extension TestCase: DRFOAuth2NodeAuthenticationClient {
+    func authenticated(node: DRFOAuth2Node) {
+        node.refreshAuthentication()
+    }
+    
+    func failedAuthenticating(node: DRFOAuth2Node, with error: Error) {
+        print("")
+    }
+    
+    func refreshedAuthentication(for node: DRFOAuth2Node) {
+        node.endAuthentication()
+    }
+    
+    func failedRefreshingAuthenticaiton(for node: DRFOAuth2Node, with error: Error) {
+        print("")
+    }
+    
+    func endedAuthentication(for node: DRFOAuth2Node) {
+        print("")
+    }
+    
     func testLoggingIn() {
         let expectation: XCTestExpectation = self.expectation(description: "bla")
         
         let node: TestOAuth2Node = .main
-        node.oauth2Handler.requestTokens(username: "ios@resmio.com", password: "password")
+        node.oauth2Clients.append(self)
+        node.authenticate(username: "ios@resmio.com", password: "password")
         
-        self.waitForExpectations(timeout: 10)
+        self.waitForExpectations(timeout: 100)
     }
 }
