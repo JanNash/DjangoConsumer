d182623f0462625b28e8dfdda1bb95d013cb28d6
Jan Nash <jnash@jnash.de>
2018-02-13 15:58:21 +0100

    Copy code from ListGettable

--- DjangoConsumer/Source/Core/Resources/DetailGettable.swift
+++ DjangoConsumer/Source/Core/Resources/DetailGettable.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+import Alamofire_SwiftyJSON
+
+
+// MARK: // Public
+// MARK: Protocol Declaration
+public protocol DetailGettable {
+    init(json: JSON)
+    var url: URL
+    static var clients: [DetailGettableClient] { get set }
+}
+
+
+// MARK: Default Implementations
+// MARK: where Self: NeedsNoAuth
+public extension DetailGettable where Self: NeedsNoAuth {
+    func get(from node: Node? = nil) {
+        self._get(from: node ?? self.defaultNode)
+    }
+}
+
+
+// MARK: // Private
+// MARK: Shared GET function Implementation
+private extension DetailGettable {
+    func _get(from node: Node) {
+        let url: URL = node.absoluteURL(for: self)
+        
+        ValidatedJSONRequest(url: url, parameters: parameters).fire(
+            via: node.sessionManager,
+            onSuccess: { result in
+                
+            },
+            onFailure: { error in
+                
+            }
+        )
+    }
+}
