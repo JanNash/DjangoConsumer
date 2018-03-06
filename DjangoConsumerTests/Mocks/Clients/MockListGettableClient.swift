2e143663576309cdf39316d5e591517f512114f1
Jan Nash <jnash@jnash.de>
2018-03-07 00:14:43 +0100

    Add LICENSE file mention to all copyright headers

--- DjangoConsumerTests/Mocks/Clients/MockListGettableClient.swift
+++ DjangoConsumerTests/Mocks/Clients/MockListGettableClient.swift
@@ -4,6 +4,9 @@
 //
 //  Created by Jan Nash on 23.01.18.
 //  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD 3 Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
 //
 
 import Foundation
