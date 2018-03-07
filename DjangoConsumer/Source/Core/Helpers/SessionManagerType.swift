c59a7a5d131935f9172d59d1dac95d9b23794217
Jan Nash <jnash@jnash.de>
2018-03-07 17:14:44 +0100

    Create SessionManagerType

---
+++ DjangoConsumer/Source/Core/Helpers/SessionManagerType.swift
@@ -0,0 +1,18 @@
+//
+//  SessionManagerType.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash (privat) on 07.03.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+import Alamofire_SwiftyJSON
+
+
+// MARK: // Public
+public protocol SessionManagerType {
+    
+}
