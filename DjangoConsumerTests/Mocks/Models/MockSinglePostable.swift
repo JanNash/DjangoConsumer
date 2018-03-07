862ada84fe12fbfabd0c16e774096c3741e38aa2
Jan Nash <jnash@jnash.de>
2018-03-07 21:29:04 +0100

    Create MockSinglePostable

---
+++ DjangoConsumerTests/Mocks/Models/MockSinglePostable.swift
@@ -0,0 +1,42 @@
+//
+//  MockSinglePostable.swift
+//  DjangoConsumerTests
+//
+//  Created by Jan Nash (privat) on 07.03.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+import DjangoConsumer
+
+
+// MARK: // Internal
+// MARK: Struct Declaration
+struct MockSinglePostable: SinglePostable, NeedsNoAuth {
+    // Init
+    init(id: String, url: String) {
+        self.id = id
+    }
+    
+    // Keys
+    struct Keys {
+        static let id: String = "id"
+    }
+    
+    // Variables
+    private(set) var id: String = "0"
+    
+    // SinglePostable
+    init(json: JSON) {
+        self.id = json[Keys.id].string!
+    }
+    
+    static var defaultNode: Node = MockNode.main
+    static var singlePostableClients: [SinglePostableClient] = []
+    
+    func toParameters() -> Parameters {
+        return [Keys.id : self.id]
+    }
+}
