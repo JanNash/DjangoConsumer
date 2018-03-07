7afbc6a5576a65b2a7ad4081f724e2488f3a4322
Jan Nash <jnash@jnash.de>
2018-03-07 17:41:39 +0100

    Implement SessionManagerType

--- DjangoConsumer/Source/Core/Helpers/SessionManagerType.swift
+++ DjangoConsumer/Source/Core/Helpers/SessionManagerType.swift
@@ -13,6 +13,19 @@ import Alamofire_SwiftyJSON
 
 
 // MARK: // Public
+// MARK: - RequestConfiguration
+public struct RequestConfiguration {
+    var url: URL
+    var method: HTTPMethod
+    var parameters: Parameters = [:]
+    var encoding: ParameterEncoding = URLEncoding.default
+    var headers: HTTPHeaders = [:]
+    var acceptableStatusCodes: [Int] = Array(200..<300)
+    var acceptableContentTypes: [String] = ["*/*"]
+}
+
+
+// MARK: - SessionManagerType
 public protocol SessionManagerType {
-    
+    func fireRequest(with configuration: RequestConfiguration, onSuccess: (JSON) -> Void, onFailure: (Error, JSON?) -> Void)
 }
