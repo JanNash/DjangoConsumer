f251b9ef5d30250dffd8a7be3962a7e405ff65c7
Jan Nash <jnash@jnash.de>
2018-03-07 17:52:35 +0100

    Improve SessionManagerType, make Alamofire.SessionManager conform to it

--- DjangoConsumer/Source/Core/Helpers/SessionManagerType.swift
+++ DjangoConsumer/Source/Core/Helpers/SessionManagerType.swift
@@ -25,7 +25,32 @@ public struct RequestConfiguration {
 }
 
 
+// MARK: - ResponseHandling
+public struct ResponseHandling {
+    var onSuccess: (JSON) -> Void
+    var onFailure: (Error) -> Void
+}
+
+
 // MARK: - SessionManagerType
 public protocol SessionManagerType {
-    func fireRequest(with configuration: RequestConfiguration, onSuccess: (JSON) -> Void, onFailure: (Error, JSON?) -> Void)
+    func fireJSONRequest(cfg: RequestConfiguration, responseHandling: ResponseHandling)
+}
+
+
+// MARK: - Alamofire SessionManager: SessionManagerType
+extension Alamofire.SessionManager: SessionManagerType {
+    public func fireJSONRequest(cfg: RequestConfiguration, responseHandling: ResponseHandling) {
+        self.request(cfg.url, method: cfg.method, parameters: cfg.parameters, encoding: cfg.encoding, headers: cfg.headers)
+            .validate(statusCode: cfg.acceptableStatusCodes)
+            .validate(contentType: cfg.acceptableContentTypes)
+            .responseSwiftyJSON {
+                switch $0.result {
+                case let .success(result):
+                    responseHandling.onSuccess(result)
+                case let .failure(error):
+                    responseHandling.onFailure(error)
+                }
+            }
+    }
 }
