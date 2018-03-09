182d742c295e94be17b6f057697d0a3a6e970883
Jan Nash <jnash@jnash.de>
2018-03-09 01:45:36 +0100

    Improve comments

--- DjangoConsumer/Source/Core/Nodes/Node.swift
+++ DjangoConsumer/Source/Core/Nodes/Node.swift
@@ -33,11 +33,15 @@ public protocol Node {
     func parametersFrom(offset: UInt, limit: UInt) -> Parameters
     func parametersFrom(filters: [FilterType]) -> Parameters
     
-    // Request URL Helpers
+    // MetaResource.Type URLs
     func relativeURL(for resourceType: MetaResource.Type, routeType: RouteType, method: HTTPMethod) -> URL
     func absoluteURL(for resourceType: MetaResource.Type, routeType: RouteType, method: HTTPMethod) -> URL
+    
+    // DetailResource URLs
     func relativeURL<T: DetailResource>(for resource: T, method: HTTPMethod) -> URL
     func absoluteURL<T: DetailResource>(for resource: T, method: HTTPMethod) -> URL
+    
+    // ResourceID URLs
     func absoluteGETURL<T>(for resourceID: ResourceID<T>) -> URL
     
     // List GET Request Helpers
@@ -83,6 +87,7 @@ public extension Node {
 
 // MARK: Request URL Helpers
 public extension Node {
+    // MetaResource.Type URLs
     func relativeURL(for resourceType: MetaResource.Type, routeType: RouteType, method: HTTPMethod) -> URL {
         return self._relativeURL(for: resourceType, routeType: routeType, method: method)
     }
@@ -91,10 +96,12 @@ public extension Node {
         return self.baseURL.appendingPathComponent(self.relativeURL(for: resourceType, routeType: routeType, method: method).absoluteString)
     }
     
+    // DetailResource URLs
     func absoluteURL<T: DetailResource>(for resource: T, method: HTTPMethod) -> URL {
         return self.baseURL.appendingPathComponent(self.relativeURL(for: resource, method: method).absoluteString)
     }
     
+    // ResourceID URLs
     func absoluteGETURL<T>(for resourceID: ResourceID<T>) -> URL {
         return self.absoluteURL(for: T.self, routeType: .detail, method: .get).appendingPathComponent(resourceID.id)
     }
