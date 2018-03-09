891a7a974cd586186698ae22ca34a7de7a1c86a3
Jan Nash <jnash@jnash.de>
2018-03-09 04:30:57 +0100

    Improve DetailGettable default implementations

--- DjangoConsumer/Source/OAuth2/Resources/DetailGettable+OAuth2.swift
+++ DjangoConsumer/Source/OAuth2/Resources/DetailGettable+OAuth2.swift
@@ -17,6 +17,6 @@ import Alamofire
 // MARK: where Self: NeedsNoAuth
 public extension DetailGettable where Self: NeedsOAuth2 {
     func get(from node: Node? = nil) {
-        self.get_(from: node ?? Self.defaultNode)
+        DefaultDetailGettableImplementations.get(self, from: node ?? Self.defaultNode)
     }
 }
