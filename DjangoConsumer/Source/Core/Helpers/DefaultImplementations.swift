5aa5e87a868f15f0e10d3679a5bfd625cd94eb03
Jan Nash <jnash@jnash.de>
2018-03-09 13:04:50 +0100

    Nest DefaultNodeImplementations

---
+++ DjangoConsumer/Source/Core/Helpers/DefaultImplementations.swift
@@ -0,0 +1,15 @@
+//
+//  DefaultImplementations.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash (privat) on 09.03.18.
+//  Copyright © 2018 Kitenow. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Public
+public struct DefaultImplementations {
+    public struct _Node_ {}
+}
