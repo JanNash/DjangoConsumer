f3b80151efd0ee193ba062c57eb58ea8abf9e62c
Jan Nash <jnash@jnash.de>
2018-03-09 20:02:13 +0100

    Update usernames in copyright headers

--- DjangoConsumer/Source/Core/Resources/MetaResource.swift
+++ DjangoConsumer/Source/Core/Resources/MetaResource.swift
@@ -2,7 +2,7 @@
 //  MetaResource.swift
 //  DjangoConsumer
 //
-//  Created by Jan Nash (privat) on 08.03.18.
+//  Created by Jan Nash on 08.03.18.
 //  Copyright © 2018 Jan Nash. All rights reserved.
 //  Published under the BSD 3 Clause license.
 //  Full license text can be found in the LICENSE file
--- DjangoConsumer/Source/Testing/TestSessionManager.swift
+++ DjangoConsumer/Source/Testing/TestSessionManager.swift
@@ -2,7 +2,7 @@
 //  TestSessionManager.swift
 //  DjangoConsumer
 //
-//  Created by Jan Nash (privat) on 07.03.18.
+//  Created by Jan Nash on 07.03.18.
 //  Copyright © 2018 Jan Nash. All rights reserved.
 //  Published under the BSD 3 Clause license.
 //  Full license text can be found in the LICENSE file
