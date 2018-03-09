e3d7addf06cc96ef6b7a061908ad0b8a8f0b660f
Jan Nash <jnash@jnash.de>
2018-03-09 13:46:45 +0100

    Improve Route

--- DjangoConsumer/Source/Core/Nodes/Route.swift
+++ DjangoConsumer/Source/Core/Nodes/Route.swift
@@ -22,8 +22,17 @@ public enum RouteType: String {
 // MARK: - Route
 // MARK: Struct Declaration
 public struct Route {
-    public var resourceType: MetaResource.Type
-    public var routeType: RouteType
-    public var method: HTTPMethod
-    public var relativeURL: URL
+    // Init
+    public init(_ resourceType: MetaResource.Type, _ routeType: RouteType, _ method: HTTPMethod, _ relativePath: String) {
+        self.resourceType = resourceType
+        self.routeType = routeType
+        self.method = method
+        self.relativeURL = URL(string: relativePath)!
+    }
+    
+    // Public Variables
+    public private(set) var resourceType: MetaResource.Type
+    public private(set) var routeType: RouteType
+    public private(set) var method: HTTPMethod
+    public private(set) var relativeURL: URL
 }
