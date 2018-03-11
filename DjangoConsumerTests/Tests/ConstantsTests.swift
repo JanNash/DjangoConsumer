3fd5538b35d53477c99cc3227198bc16c80cd5eb
Jan Nash <jnash@jnash.de>
2018-03-12 00:42:08 +0100

    Add more tests for hardcoded constants

--- DjangoConsumerTests/Tests/ConstantsTests.swift
+++ DjangoConsumerTests/Tests/ConstantsTests.swift
@@ -10,7 +10,8 @@
 //
 
 import XCTest
-import DjangoConsumer
+import Alamofire
+@testable import DjangoConsumer
 
 
 // MARK: // Internal
@@ -41,6 +42,12 @@ class ConstantsTests: BaseTest {
         XCTAssertEqual(FixtureType.put.rawValue, "PUT")
         XCTAssertEqual(FixtureType.patch.rawValue, "PATCH")
         XCTAssertEqual(FixtureType.delete.rawValue, "DELETE")
+        
+        let allResourceHTTPMethods: [FixtureType] = FixtureType.all
+        XCTAssertEqual(allResourceHTTPMethods, [.get, .head, .post, .put, .patch, .delete])
+        
+        let allHTTPMethods: [HTTPMethod] = [.get, .head, .post, .put, .patch, .delete]
+        XCTAssertEqual(allResourceHTTPMethods.map({ $0.toHTTPMethod() }), allHTTPMethods)
     }
     
     func testOAuth2Constants() {
@@ -73,5 +80,33 @@ class ConstantsTests: BaseTest {
         
         XCTAssertEqual(FixtureType.detail.rawValue, "detail")
         XCTAssertEqual(FixtureType.list.rawValue, "list")
+    }
+    
+    func testFilterKeyConstants() {
+        typealias FixtureType = FilterKey<Any>.DefaultFilterKeys
+        
+        XCTAssertEqual(FixtureType.date.rawValue, "date")
+        XCTAssertEqual(FixtureType.id.rawValue, "id")
+        XCTAssertEqual(FixtureType.name.rawValue, "name")
+        
+        let date: FilterKey = .date
+        XCTAssertEqual(date.string, "date")
+        
+        let _id: FilterKey = .id
+        XCTAssertEqual(_id.string, "id")
+        
+        let name: FilterKey = .name
+        XCTAssertEqual(name.string, "name")
+    }
+    
+    func testFilterComparatorConstants() {
+        typealias FixtureType = FilterComparator.DefaultFilterComparators
+        
+        XCTAssertEqual(FixtureType.__gt.rawValue, "__gt")
+        XCTAssertEqual(FixtureType.__gte.rawValue, "__gte")
+        XCTAssertEqual(FixtureType.__icontains.rawValue, "__icontains")
+        XCTAssertEqual(FixtureType.__in.rawValue, "__in")
+        XCTAssertEqual(FixtureType.__lt.rawValue, "__lt")
+        XCTAssertEqual(FixtureType.__lte.rawValue, "__lte")
     }
 }
