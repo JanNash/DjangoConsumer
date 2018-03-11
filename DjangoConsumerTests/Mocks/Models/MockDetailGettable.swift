d633380c3e80be228c8a8e8586079c2d09d4d0ef
Jan Nash <jnash@jnash.de>
2018-03-11 23:58:28 +0100

    Create MockDetailGettable

---
+++ DjangoConsumerTests/Mocks/Models/MockDetailGettable.swift
@@ -0,0 +1,38 @@
+//
+//  MockDetailGettable.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 11.03.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+import DjangoConsumer
+
+
+// MARK: // Internal
+// MARK: Struct Declaration
+struct MockDetailGettable: DetailGettable, NeedsNoAuth {
+    // Keys
+    struct Keys {
+        static let id: String = "id"
+    }
+    
+    // SinglePostable
+    init(json: JSON) {
+        self.id = ResourceID(json[Keys.id].string!)
+    }
+    
+    static var defaultNode: Node = MockNode.main
+    static var detailGettableClients: [DetailGettableClient] = []
+    
+    var id: ResourceID<MockDetailGettable>
+    
+    func gotNewSelf(_ newSelf: MockDetailGettable, from: Node) {}
+    func failedGettingNewSelf(from: Node, with error: Error) {}
+}
