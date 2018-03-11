0b25d05268fb282ff7ef8e7380ad0502782ee5ee
Jan Nash <jnash@jnash.de>
2018-03-11 16:56:03 +0100

    Add NodeTests file

---
+++ DjangoConsumerTests/Tests/NodeTests.swift
@@ -0,0 +1,19 @@
+//
+//  NodeTests.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 11.03.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import XCTest
+import DjangoConsumer
+
+
+// MARK: // Internal
+class NodeTests: BaseTest {
+    
+}
