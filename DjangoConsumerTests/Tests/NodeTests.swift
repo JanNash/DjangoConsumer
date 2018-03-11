1e13176e2fe66f47cf59a8465e8cc17be19232a7
Jan Nash <jnash@jnash.de>
2018-03-11 17:25:59 +0100

    Add first tests for Node

--- DjangoConsumerTests/Tests/NodeTests.swift
+++ DjangoConsumerTests/Tests/NodeTests.swift
@@ -10,10 +10,29 @@
 //
 
 import XCTest
+import Alamofire
 import DjangoConsumer
 
 
 // MARK: // Internal
 class NodeTests: BaseTest {
+    func testDefaultFilters() {
+        let node: Node = MockNode()
+        XCTAssert(node.defaultFilters(for: MockFilteredListGettable.self).isEmpty)
+    }
     
+    func testParametersFromOffsetLimitAndFilters() {
+        let node: Node = MockNode()
+        let expectedOffset: UInt = 10
+        let expectedLimit: UInt = 100
+        let nameFilter: _F<String> = _F(.name, .__icontains, "blubb")
+        let filters = [nameFilter]
+        
+        let parameters: Parameters = node.parametersFrom(offset: expectedOffset, limit: expectedLimit, filters: filters)
+        
+        XCTAssert(parameters.count == 3)
+        XCTAssertEqual(parameters[DefaultPagination.Keys.offset] as? UInt, expectedOffset)
+        XCTAssertEqual(parameters[DefaultPagination.Keys.limit] as? UInt, expectedLimit)
+        XCTAssertEqual(parameters[nameFilter.stringKey] as? String, nameFilter.value as? String)
+    }
 }
