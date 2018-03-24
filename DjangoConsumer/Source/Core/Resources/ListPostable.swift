51c3f48ae56c783b0f572e215499284fd63ac5d7
Jan Nash <jnash@jnash.de>
2018-03-24 21:56:50 +0100

    Use ParameterConvertible where appropriate

--- DjangoConsumer/Source/Core/Resources/ListPostable.swift
+++ DjangoConsumer/Source/Core/Resources/ListPostable.swift
@@ -18,7 +18,7 @@ import Alamofire_SwiftyJSON
 // MARK: // Public
 // MARK: - ListPostable
 // MARK: Protocol Declaration
-public protocol ListPostable: ListResource, JSONInitializable {
+public protocol ListPostable: ListResource, JSONInitializable, ParameterConvertible {
     static var listPostableClients: [ListPostableClient] { get set }
 }
 
