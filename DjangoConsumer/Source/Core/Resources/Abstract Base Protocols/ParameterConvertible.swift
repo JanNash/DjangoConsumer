2749b11e897d2c280c38282cb6b1ccbeb8143a7d
Jan Nash <jnash@jnash.de>
2018-03-24 21:55:53 +0100

    Create ParameterConvertible Protocol

---
+++ DjangoConsumer/Source/Core/Resources/Abstract Base Protocols/ParameterConvertible.swift
@@ -0,0 +1,19 @@
+//
+//  ParameterConvertible.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 24.03.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import Foundation
+import Alamofire
+
+
+// MARK: // Public
+public protocol ParameterConvertible {
+    func toParameters() -> Parameters
+}
