a5da1435feb7650c6014131440ee98e37b8a2e72
Jan Nash <jnash@jnash.de>
2018-03-24 14:53:10 +0100

    Update XCTestCase extension implementation

--- DjangoConsumerTests/Helpers/ProgrammerAssertions.swift
+++ DjangoConsumerTests/Helpers/ProgrammerAssertions.swift
@@ -17,19 +17,19 @@ import Foundation
 // MARK: // Internal
 // MARK: Assertion Functions
 func assert(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) {
-    Assertions.assert(condition(), message(), file, line)
+    ProgrammerAssertions.assert(condition(), message(), file, line)
 }
 
 func precondition(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) {
-    Assertions.precondition(condition(), message(), file, line)
+    ProgrammerAssertions.precondition(condition(), message(), file, line)
 }
 
 func assertionFailure(_ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) {
-    Assertions.assertionFailure(message(), file, line)
+    ProgrammerAssertions.assertionFailure(message(), file, line)
 }
 
 func preconditionFailure(_ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) -> Never {
-    Assertions.preconditionFailure(message(), file, line)
+    ProgrammerAssertions.preconditionFailure(message(), file, line)
     // This is needed to satisfy the return type `Never`, since `AssertFunction` returns Void
     // It will never be executed, since a `repeat { RunLoop.main.run() } while (true)` is
     // injected into the custom assertions in `XCTestCase (ProgrammerAssertions).swift`.
@@ -40,7 +40,7 @@ func preconditionFailure(_ message: @autoclosure () -> String = "", file: Static
 }
 
 func fatalError(_ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) -> Never {
-    Assertions.fatalError(message(), file, line)
+    ProgrammerAssertions.fatalError(message(), file, line)
     // This is needed to satisfy the return type `Never`, since `AssertFunction` returns Void
     // It will never be executed, since a `repeat { RunLoop.main.run() } while (true)` is
     // injected into the custom assertions in `XCTestCase (ProgrammerAssertions).swift`.
@@ -52,30 +52,30 @@ func fatalError(_ message: @autoclosure () -> String = "", file: StaticString = 
 
 
 // MARK: // Assertions
-struct Assertions {
-    typealias Assertion = (String, StaticString, UInt) -> Void
+struct ProgrammerAssertions {
+    typealias StaticAssertion = (String, StaticString, UInt) -> Void
     typealias ConditionalAssertion = (Bool, String, StaticString, UInt) -> Void
     
     // Custom Assertions
-    static var assert: ConditionalAssertion         = Assertions.swiftAssert
-    static var precondition: ConditionalAssertion   = Assertions.swiftPrecondition
-    static var assertionFailure: Assertion          = Assertions.swiftAssertionFailure
-    static var preconditionFailure: Assertion       = Assertions.swiftPreconditionFailure
-    static var fatalError: Assertion                = Assertions.swiftFatalError
+    static var assert: ConditionalAssertion         = ProgrammerAssertions.swiftAssert
+    static var precondition: ConditionalAssertion   = ProgrammerAssertions.swiftPrecondition
+    static var assertionFailure: StaticAssertion    = ProgrammerAssertions.swiftAssertionFailure
+    static var preconditionFailure: StaticAssertion = ProgrammerAssertions.swiftPreconditionFailure
+    static var fatalError: StaticAssertion          = ProgrammerAssertions.swiftFatalError
     
     // Swift Assertions
-    static let swiftAssert: ConditionalAssertion        = { Swift.assert($0, $1, file: $2, line: $3) }
-    static let swiftPrecondition: ConditionalAssertion  = { Swift.precondition($0, $1, file: $2, line: $3) }
-    static let swiftAssertionFailure: Assertion         = { Swift.assertionFailure($0, file: $1, line: $2) }
-    static let swiftPreconditionFailure: Assertion      = { Swift.preconditionFailure($0, file: $1, line: $2) }
-    static let swiftFatalError: Assertion               = { Swift.fatalError($0, file: $1, line: $2) }
+    static let swiftAssert: ConditionalAssertion            = { Swift.assert($0, $1, file: $2, line: $3) }
+    static let swiftPrecondition: ConditionalAssertion      = { Swift.precondition($0, $1, file: $2, line: $3) }
+    static let swiftAssertionFailure: StaticAssertion       = { Swift.assertionFailure($0, file: $1, line: $2) }
+    static let swiftPreconditionFailure: StaticAssertion    = { Swift.preconditionFailure($0, file: $1, line: $2) }
+    static let swiftFatalError: StaticAssertion             = { Swift.fatalError($0, file: $1, line: $2) }
     
     // Reset Functions
-    static func resetAssert()               { Assertions.assert = Assertions.swiftAssert }
-    static func resetAssertionFailure()     { Assertions.assertionFailure = Assertions.swiftAssertionFailure }
-    static func resetPrecondition()         { Assertions.precondition = Assertions.swiftPrecondition }
-    static func resetPreconditionFailure()  { Assertions.preconditionFailure = Assertions.swiftPreconditionFailure }
-    static func resetFatalError()           { Assertions.fatalError = Assertions.swiftFatalError }
+    static func resetAssert()               { ProgrammerAssertions.assert = ProgrammerAssertions.swiftAssert }
+    static func resetAssertionFailure()     { ProgrammerAssertions.assertionFailure = ProgrammerAssertions.swiftAssertionFailure }
+    static func resetPrecondition()         { ProgrammerAssertions.precondition = ProgrammerAssertions.swiftPrecondition }
+    static func resetPreconditionFailure()  { ProgrammerAssertions.preconditionFailure = ProgrammerAssertions.swiftPreconditionFailure }
+    static func resetFatalError()           { ProgrammerAssertions.fatalError = ProgrammerAssertions.swiftFatalError }
 }
 
 #endif
