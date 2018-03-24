db7c285e5bfe5ca1c6b0f981cae49aaae826879b
Jan Nash <jnash@jnash.de>
2018-03-24 13:57:19 +0100

    Add XCTestCase extension file

---
+++ DjangoConsumerTests/Helpers/XCTestCase (ProgrammerAssertions).swift
@@ -0,0 +1,12 @@
+//
+//  XCTestCase (ProgrammerAssertions).swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 24.03.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import Foundation
