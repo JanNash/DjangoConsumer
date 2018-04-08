82a916a76ef5ffd1943c4145897a748afe261f7e
Jan Nash <jnash@jnash.de>
2018-04-08 12:45:09 +0200

    Add MockError for testing

---
+++ DjangoConsumerTests/Mocks/MockError.swift
@@ -0,0 +1,19 @@
+//
+//  MockError.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 08.04.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import Foundation
+
+
+// MARK: // Internal
+enum MockError: Error {
+    case foo
+    case bar
+}
