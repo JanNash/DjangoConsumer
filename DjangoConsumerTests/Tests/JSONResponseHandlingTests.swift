7fa2d2818c3368d3fab2327640532a65c9144879
Jan Nash <jnash@jnash.de>
2018-04-08 12:46:29 +0200

    Add tests for JSONRepsonseHandling init

---
+++ DjangoConsumerTests/Tests/JSONResponseHandlingTests.swift
@@ -0,0 +1,52 @@
+//
+//  JSONResponseHandlingTests.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 08.04.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import XCTest
+import Alamofire
+import SwiftyJSON
+import Alamofire_SwiftyJSON
+import DjangoConsumer
+
+
+
+
+// MARK: // Internal
+class JSONResponseHandlingTests: BaseTest {
+    func testInitOnSuccessCorrectlyStored() {
+        let expectation: XCTestExpectation = self.expectation(
+            description: "Expected onSuccess closure that was passed into init to be called"
+        )
+        
+        let responseHandling: JSONResponseHandling = JSONResponseHandling(
+            onSuccess: { _ in expectation.fulfill() },
+            onFailure: { _ in XCTFail("onFailure closure that was passed into init shouldn't be called") }
+        )
+        
+        responseHandling.onSuccess(JSON())
+        
+        self.waitForExpectations(timeout: 10)
+    }
+    
+    func testInitOnFailureCorrectlyStored() {
+        let expectation: XCTestExpectation = self.expectation(
+            description: "Expected onFailure closure that was passed into init to be called"
+        )
+        
+        let responseHandling: JSONResponseHandling = JSONResponseHandling(
+            onSuccess: { _ in XCTFail("onSuccess closure that was passed into init shouldn't be called") },
+            onFailure: { _ in expectation.fulfill() }
+        )
+        
+        responseHandling.onFailure(MockError.foo)
+        
+        self.waitForExpectations(timeout: 10)
+    }
+}
