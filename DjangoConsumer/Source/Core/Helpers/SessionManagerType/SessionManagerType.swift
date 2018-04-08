a6fa27de2df896ef864f7c66f4d7a34eb3efb510
Jan Nash <jnash@jnash.de>
2018-04-08 12:10:31 +0200

    Extract RequestConfiguration into separate file

--- DjangoConsumer/Source/Core/Helpers/SessionManagerType/SessionManagerType.swift
+++ DjangoConsumer/Source/Core/Helpers/SessionManagerType/SessionManagerType.swift
@@ -16,28 +16,6 @@ import Alamofire_SwiftyJSON
 
 
 // MARK: // Public
-// MARK: - RequestConfiguration
-public struct RequestConfiguration {
-    public init(url: URL, method: ResourceHTTPMethod, parameters: Parameters = [:], encoding: ParameterEncoding, headers: HTTPHeaders = [:], acceptableStatusCodes: [Int] = Array(200..<300), acceptableContentTypes: [String] = ["*/*"]) {
-        self.url = url
-        self.method = method
-        self.parameters = parameters
-        self.encoding = encoding
-        self.headers = headers
-        self.acceptableStatusCodes = acceptableStatusCodes
-        self.acceptableContentTypes = acceptableContentTypes
-    }
-    
-    public var url: URL
-    public var method: ResourceHTTPMethod
-    public var parameters: Parameters
-    public var encoding: ParameterEncoding
-    public var headers: HTTPHeaders
-    public var acceptableStatusCodes: [Int]
-    public var acceptableContentTypes: [String]
-}
-
-
 // MARK: - JSONResponseHandling
 public struct JSONResponseHandling {
     // Inits
