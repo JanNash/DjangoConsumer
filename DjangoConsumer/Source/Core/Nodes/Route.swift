0d428f484bedcb39fc850cb479749e5713b05d3b
Jan Nash <jnash@jnash.de>
2018-04-25 11:47:28 +0200

    Update Route implementation to new RouteType interface

--- DjangoConsumer/Source/Core/Nodes/Route.swift
+++ DjangoConsumer/Source/Core/Nodes/Route.swift
@@ -52,17 +52,15 @@ public struct RouteType: Equatable {
 // MARK: Struct Declaration
 public struct Route {
     // Private Init
-    private init(_ resourceType: MetaResource.Type, _ routeType: RouteType, _ method: ResourceHTTPMethod, _ relativePath: String) {
+    private init(_ resourceType: MetaResource.Type, _ routeType: RouteType, _ relativePath: String) {
         self.resourceType = resourceType
         self.routeType = routeType
-        self.method = method
         self.relativeURL = URL(string: relativePath)!
     }
     
     // Public Variables
     public private(set) var resourceType: MetaResource.Type
     public private(set) var routeType: RouteType
-    public private(set) var method: ResourceHTTPMethod
     public private(set) var relativeURL: URL
 }
 
@@ -79,7 +77,7 @@ extension Route: Equatable {
 // MARK: Hashable
 extension Route: Hashable {
     public var hashValue: Int {
-        return "\(self.resourceType)-\(self.routeType)-\(self.method)".hashValue
+        return "\(self.resourceType)-\(self.routeType.numerus)-\(self.routeType.method)".hashValue
     }
 }
 
@@ -88,11 +86,11 @@ extension Route: Hashable {
 // MARK: GET
 public extension Route {
     public static func listGET<T>(_ t: T.Type, _ rel: String) -> Route where T: ListGettable {
-        return Route(t, .list, .get, rel)
+        return Route(t, .listGET, rel)
     }
     
     public static func detailGET<T>(_ t: T.Type, _ rel: String) -> Route where T: DetailGettable {
-        return Route(t, .detail, .get, rel)
+        return Route(t, .detailGET, rel)
     }
 }
 
@@ -100,11 +98,11 @@ public extension Route {
 // MARK: POST
 public extension Route {
     public static func listPOST<T>(_ t: T.Type, _ rel: String) -> Route where T: ListPostable {
-        return Route(t, .list, .post, rel)
+        return Route(t, .listPOST, rel)
     }
     
     public static func singlePOST<T>(_ t: T.Type, _ rel: String) -> Route where T: SinglePostable {
-        return Route(t, .detail, .post, rel)
+        return Route(t, .singlePOST, rel)
     }
 }
 
@@ -112,7 +110,7 @@ public extension Route {
 // MARK: PUT
 //public extension Route {
 //    public static func detailPUT<T>(_ t: T.Type, _ rel: String) -> Route where T: DetailPuttable {
-//        return Route(t, .detail, .put, rel)
+//        return Route(t, .detailPUT, rel)
 //    }
 //}
 
@@ -120,7 +118,7 @@ public extension Route {
 // MARK: PATCH
 //public extension Route {
 //    public static func detailPATCH<T>(_ t: T.Type, _ rel: String) -> Route where T: DetailPatchable {
-//        return Route(t, .detail, .patch, rel)
+//        return Route(t, .detailPATCH, rel)
 //    }
 //}
 
@@ -128,7 +126,7 @@ public extension Route {
 // MARK: DELETE
 //public extension Route {
 //    public static func detailDELETE<T>(_ t: T.Type, _ rel: String) -> Route where T: DetailDeletable {
-//        return Route(t, .detail, .delete, rel)
+//        return Route(t, .detailDELETE, rel)
 //    }
 //}
 
@@ -136,7 +134,7 @@ public extension Route {
 // MARK: // Internal
 // MARK: Matching function
 extension Route {
-    static func matches(_ resourceType: MetaResource.Type, _ routeType: RouteType, _ method: ResourceHTTPMethod) -> (Route) -> Bool {
-        return { return $0.resourceType == resourceType && $0.routeType == routeType && $0.method == method }
+    static func matches(_ resourceType: MetaResource.Type, _ routeType: RouteType) -> (Route) -> Bool {
+        return { return $0.resourceType == resourceType && $0.routeType == routeType }
     }
 }
