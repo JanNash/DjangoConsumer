d3ca5964a832ec9a14f89f5ee7654fa067c282ca
Jan Nash <jnash@jnash.de>
2018-04-27 23:13:09 +0200

    Split DetailResource file

---
+++ DjangoConsumer/Source/Core/Resources/Abstract Base Protocols/IdentifiableResource.swift
@@ -0,0 +1,25 @@
+//
+//  IdentifiableResource.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 27.04.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import Foundation
+
+
+// MARK: // Public
+// MARK: - IdentifiableResource
+public protocol IdentifiableResource: DetailResource {
+    var id: ResourceID<Self>? { get }
+}
+
+
+// MARK: - IdentifiableResourceError
+public enum IdentifiableResourceError: Error {
+    case hasNoID
+}
