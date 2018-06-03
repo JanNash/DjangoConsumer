592b297004c2c2f25707b084e589123c24541e13
Jan Nash <jnash@jnash.de>
2018-06-03 16:53:28 +0200

    Make RequestPayload equatable

--- DjangoConsumer/Source/Core/Helpers/SessionManagerType/RequestPayload.swift
+++ DjangoConsumer/Source/Core/Helpers/SessionManagerType/RequestPayload.swift
@@ -14,27 +14,60 @@ import Foundation
 
 // MARK: // Public
 // MARK: -
-public enum RequestPayload {
+public enum RequestPayload: Equatable {
     case json(JSONDict)
     case multipart([FormData])
 }
 
 
+//extension Array: RequestPayloadConvertible where Element: RequestPayloadConvertible {
+//    public func toPayload(for method: ResourceHTTPMethod) -> RequestPayload {
+//        let payloads: [RequestPayload] = self.map({ $0.toPayload(for: method) })
+//        if payloads.contains(.multipart) {
+//
+//        }
+//    }
+//}
+
+
+// MARK: - Array where Element == (String, RequestPayload)
+extension Array where Element == (String, RequestPayload) {
+    public static func == (lhs: [Element], rhs: [Element]) -> Bool {
+        guard lhs.count == rhs.count else { return false }
+        for (i, lElement) in lhs.enumerated() {
+            guard lElement == rhs[i] else {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+
 // MARK: -
-public enum FormData {
+public enum FormData: Equatable {
     case json(JSONDict)
     case image(key: String, image: UIImage, mimeType: MimeType.Image)
     case nested([(String, RequestPayload)])
+    
+    public static func == (lhs: FormData, rhs: FormData) -> Bool {
+        switch (lhs, rhs) {
+        case (.json(let l), .json(let r)):      return l == r
+        case (.image(let l), .image(let r)):    return l == r
+        case (.nested(let l), .nested(let r)):  return l == r
+        default:                                return false
+        }
+    }
 }
 
 
 // MARK: -
 public enum MimeType {
-    public enum Application {
+    public enum Application: Equatable {
         case json
     }
     
-    public enum Image {
+    public enum Image: Equatable {
         case jpeg
         case png
     }
