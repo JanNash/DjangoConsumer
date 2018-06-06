9873293c8e0b59c5cd09ab79943566d5ff08bcfa
Jan Nash <jnash@jnash.de>
2018-06-06 23:49:15 +0200

    Make request creation of SessionManagerType async

--- DjangoConsumer/Source/Core/Helpers/SessionManagerType/SessionManagerType.swift
+++ DjangoConsumer/Source/Core/Helpers/SessionManagerType/SessionManagerType.swift
@@ -13,9 +13,17 @@ import Alamofire
 
 
 // MARK: // Public
+// MARK: -
+public enum RequestCreationResult {
+    case created(DataRequest)
+    case failed(Error)
+}
+
+
+// MARK: -
 public protocol SessionManagerType: class {
-    func createRequest(with cfg: GETRequestConfiguration) -> DataRequest
-    func createRequest(with cfg: POSTRequestConfiguration) -> DataRequest
+    func createRequest(with cfg: GETRequestConfiguration, completion: (RequestCreationResult) -> Void)
+    func createRequest(with cfg: POSTRequestConfiguration, completion: (RequestCreationResult) -> Void)
     func handleJSONResponse(for request: DataRequest, with responseHandling: JSONResponseHandling)
 }
 
@@ -29,16 +37,33 @@ public extension SessionManagerType {
     func fireRequest(with cfg: POSTRequestConfiguration, responseHandling: JSONResponseHandling) {
         DefaultImplementations.SessionManagerType.fireRequest(via: self, with: cfg, responseHandling: responseHandling)
     }
+    
+    func handle(requestCreationResult: RequestCreationResult, responseHandling: JSONResponseHandling) {
+        DefaultImplementations.SessionManagerType.handle(requestCreationResult: requestCreationResult, of: self, responseHandling: responseHandling)
+    }
 }
 
 
 // MARK: - DefaultImplementations.SessionManagerType
 public extension DefaultImplementations.SessionManagerType {
     static func fireRequest(via sessionManager: SessionManagerType, with cfg: GETRequestConfiguration, responseHandling: JSONResponseHandling) {
-        sessionManager.handleJSONResponse(for: sessionManager.createRequest(with: cfg), with: responseHandling)
+        sessionManager.createRequest(with: cfg) {
+            sessionManager.handle(requestCreationResult: $0, responseHandling: responseHandling)
+        }
     }
     
     static func fireRequest(via sessionManager: SessionManagerType, with cfg: POSTRequestConfiguration, responseHandling: JSONResponseHandling) {
-        sessionManager.handleJSONResponse(for: sessionManager.createRequest(with: cfg), with: responseHandling)
+        sessionManager.createRequest(with: cfg) {
+            sessionManager.handle(requestCreationResult: $0, responseHandling: responseHandling)
+        }
+    }
+    
+    static func handle(requestCreationResult: RequestCreationResult, of sessionManager: SessionManagerType, responseHandling: JSONResponseHandling) {
+        switch requestCreationResult {
+        case .created(let request):
+            sessionManager.handleJSONResponse(for: request, with: responseHandling)
+        case .failed(let error):
+            responseHandling.onFailure(error)
+        }
     }
 }
