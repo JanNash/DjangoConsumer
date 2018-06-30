caf36348b3348caaa7d6fa68a5422ef9c4cdd891
Jan Nash <jnash@jnash.de>
2018-06-30 22:39:31 +0200

    Update ListPostable implementation

--- DjangoConsumer/Source/Core/Resources/ListPostable.swift
+++ DjangoConsumer/Source/Core/Resources/ListPostable.swift
@@ -53,8 +53,7 @@ private extension DefaultImplementations.ListPostable {
     static func _post<C: Collection, T: ListPostable>(_ objects: C, to node: Node, via sessionManager: SessionManagerType) where C.Element == T {
         let routeType: RouteType.List = .listPOST
         let url: URL = node.absoluteURL(for: T.self, routeType: routeType)
-        let payload: RequestPayload = node.payloadFrom(listPostables: objects)
-        let unwrappedPayload: RequestPayload.Unwrapped = node.unwrapPayload(payload, for: objects)
+        let payload: Payload = node.payloadFrom(listPostables: objects)
         let encoding: ParameterEncoding = URLEncoding.default
         
         func onSuccess(_ json: JSON) {
@@ -67,7 +66,7 @@ private extension DefaultImplementations.ListPostable {
         }
         
         sessionManager.fireRequest(
-            with: .post(POSTRequestConfiguration(url: url, payload: unwrappedPayload, encoding: encoding)),
+            with: .post(POSTRequestConfiguration(url: url, payload: payload, encoding: encoding)),
             responseHandling: JSONResponseHandling(onSuccess: onSuccess, onFailure: onFailure)
         )
     }
