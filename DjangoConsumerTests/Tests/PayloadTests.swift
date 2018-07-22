d28cb0bcf0c6bb5535133ec263858f7ba95f78c9
Jan Nash <jnash@jnash.de>
2018-07-23 00:02:57 +0200

    Write first test for Payload

---
+++ DjangoConsumerTests/Tests/PayloadTests.swift
@@ -0,0 +1,42 @@
+//
+//  PayloadTests.swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash on 22.07.18.
+//  Copyright © 2018 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import XCTest
+@testable import DjangoConsumer
+
+
+public func == (_ lhs: Payload.Multipart.UnwrappedPayload, _ rhs: Payload.Multipart.UnwrappedPayload) -> Bool {
+    return true
+}
+
+
+// MARK: // Internal
+class PayloadTests: XCTestCase {
+    func testPayloadFromMultipartArrayWithSomeValuesThatProduceNilDataAndSomeThatProduceActualData() {
+        let payloadDict: Payload.Dict = Payload.Dict([
+            "images": [
+                UIImage(), // UIImagePNGRepresentation() will produce nil data for this
+                UIImage(color: .clear) // UIImagePNGRepresentation() will produce actual data for this
+            ]
+        ])
+        
+        let payload: Payload = payloadDict.toPayload(conversion: DefaultPayloadConversion(), rootObject: nil, method: .post)
+        
+        XCTAssert(payload.json.isEmpty)
+        
+        let expectedMultipartPayload: Payload.Multipart.UnwrappedPayload = [
+            "images[0]": Payload.Multipart.ContentType.imagePNG.null,
+            "images[1]": (UIImagePNGRepresentation(UIImage(color: .clear))!, .imagePNG)
+        ]
+        
+        XCTAssert(payload.multipart == expectedMultipartPayload)
+    }
+}
