489fcc862f39785f348e52deea772219b94a4a6a
Jan Nash <jnash@jnash.de>
2018-07-23 21:28:41 +0200

    Update Array: PayloadElementConvertible implementation to make test pass

--- DjangoConsumer/Source/Core/Helpers/Payload/Convertible Implementations/PayloadElementConvertibles/Array (PayloadElementConvertible).swift
+++ DjangoConsumer/Source/Core/Helpers/Payload/Convertible Implementations/PayloadElementConvertibles/Array (PayloadElementConvertible).swift
@@ -24,10 +24,33 @@ extension Array: PayloadElementConvertible where Element: PayloadElementConverti
 // MARK: : PayloadElementConvertible Implementation
 private extension Array/*: PayloadElementConvertible*/ where Element: PayloadElementConvertible {
     func _toPayloadElement(conversion: PayloadConversion, configuration: PayloadConversion.Configuration) -> Payload.Element {
-        var jsonArray: [Any] = []
+        let currentKey: String = configuration.currentKey
         var multipartPayload: Payload.Multipart.UnwrappedPayload = [:]
         
-        let currentKey: String = configuration.currentKey
+        if let multipartConvertibleArray: [MultipartValueConvertible] = self as? [MultipartValueConvertible] {
+            multipartConvertibleArray.enumerated().forEach({
+                let payloadElement: Payload.Element = DefaultImplementations.MultipartValueConvertible.payloadElement(
+                    from: $0.element,
+                    conversion: (
+                        conversion,
+                        (
+                            rootObject: configuration.rootObject,
+                            method: configuration.method,
+                            multipartPath: configuration.multipartPath + .index($0.offset),
+                            currentKey: currentKey
+                        )
+                    )
+                )
+                
+                if let multipart: Payload.Multipart.UnwrappedPayload = payloadElement.multipart {
+                    multipartPayload.merge(multipart, strategy: .overwriteOldValue)
+                }
+            })
+            
+            return (nil, multipartPayload)
+        }
+        
+        var jsonArray: [Any] = []
         
         self.enumerated().forEach({
             let payloadElement: Payload.Element = $0.element.toPayloadElement(
