deea5e644ea6163f2643b78b13b9d1265f930854
Jan Nash <jnash@jnash.de>
2020-04-27 17:16:51 +0200

    Create test for Dictionary PayloadElementConvertible implementation

---
+++ DjangoConsumerTests/Tests/Core/Helpers/Payload/Convertible Implementations/PayloadElementConvertibles/Dictionary (PayloadElementConvertible).swift
@@ -0,0 +1,44 @@
+//
+//  Dictionary (PayloadElementConvertible).swift
+//  DjangoConsumer
+//
+//  Created by Jan Nash (resmio) on 27.04.20.
+//  Copyright © 2020 Jan Nash. All rights reserved.
+//  Published under the BSD-3-Clause license.
+//  Full license text can be found in the LICENSE file
+//  at the root of this repository.
+//
+
+import XCTest
+import DjangoConsumer
+
+
+// MARK: -
+class Dictionary_PayloadElementConvertible_Tests: BaseTest {
+    func testNestedDictionaryPureJSON() {
+        let currentKey: String = "foo"
+        let dict: Payload.Dict = ["bar": ["baz": true, "zoing": false]]
+        
+        let payloadElement: Payload.Element = dict.toPayloadElement(
+            conversion: DefaultPayloadConversion(),
+            configuration: (
+                rootObject: nil,
+                method: .get,
+                multipartPath: Payload.Multipart.Path(currentKey),
+                currentKey: currentKey
+            )
+        )
+        
+//        guard payloadElement.multipart == nil else {
+//            XCTFail("payloadElement should not contain multipart")
+//            return
+//        }
+        
+        guard let json: Payload.JSON.UnwrappedPayload = payloadElement.json else {
+            XCTFail("payloadElement should contain json")
+            return
+        }
+        
+        XCTAssert(json == ["foo": ["bar": ["baz": true, "zoing": false]]])
+    }
+}
